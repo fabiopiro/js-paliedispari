1b193221bfc8e951c0426634497abdb97e93c94a
fabiopiro <piroddifabio01@gmail.com>
2021-05-03 15:03:19 +0200

    pari e dispari

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -3,3 +3,16 @@ Palindroma
 Chiedere all'utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
+
+// Chiedere all'utente di inserire una parola
+var userWord = prompt("Inserisci una parola");
+console.log(userWord);
+
+
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
+
+function isPalindroma () {
+    
+}
--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -6,3 +6,58 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
+
+// L'utente sceglie pari o dispari...
+var userChoice = prompt("Scegli pari o dispari");
+console.log(userChoice);
+
+if (userChoice == "pari") {
+    userChoice = true;
+} else {
+    userChoice = false
+}
+
+console.log("pari = ", userChoice);
+
+
+// ...e inserisce un numero da 1 a 5.
+var userNumber = parseInt(prompt("Inserisci un numero tra 1 e 5"));
+
+console.log("Tuo Numero", userNumber);
+
+
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+function randomNumber() {
+    
+    return Math.floor(Math.random() * 5 + 1);
+
+}
+
+var computerNumber = randomNumber();
+console.log("Numero del Computer: ", computerNumber);
+
+
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+function isSumEven(numUno, numDue) {
+    
+    sum = numUno + numDue;
+    
+    if ((sum % 2) == 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+sumUserComputer = isSumEven(userNumber, computerNumber);
+console.log("La somma dei due numeri è", sum);
+console.log(sum + " è pari? " + sumUserComputer);
+
+
+// Dichiariamo chi ha vinto.
+if (userChoice == sumUserComputer) {
+    console.log("HAI VINTO!");
+} else {
+    console.log("HAI PERSO!");
+}
